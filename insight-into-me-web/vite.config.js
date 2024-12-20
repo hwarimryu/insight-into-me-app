b454cd91a33262a5c867aa5eb9467a7e5e9098fc
hwarimryu <fwarim@gmail.com>
2024-12-21 00:08:13 +0900

    pwa로 만들기

--- insight-into-me-web/vite.config.js
+++ insight-into-me-web/vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "./", // 상대 경로 설정
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Timeline Management App",
+        short_name: "Timeline",
+        description: "A timeline management application.",
+        theme_color: "#3182ce",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
